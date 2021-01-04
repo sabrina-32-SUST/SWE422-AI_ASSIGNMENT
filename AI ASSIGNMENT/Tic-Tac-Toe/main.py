9895df86f08fb02310d7395d328fe8db8f1da975
Sabrina <sanutaayvi@gmail.com>
2021-01-04 20:48:25 +0600

    gameOver  function

--- AI ASSIGNMENT/Tic-Tac-Toe/main.py
+++ AI ASSIGNMENT/Tic-Tac-Toe/main.py
@@ -11,11 +11,24 @@ board = [
 
 ]
 "minimax fuction  will  choose   the  best  move"
+
+
+def gameOver(state):
+    pass
+
+
+def evaluate(state):
+    pass
+
+
 def minimax(state, depth, player):
     if player == computer:
         best = [-1, -1, -infinity]
     else:
         best = [-1, -1, +infinity]
+    if depth == 0 or gameOver(state):
+        score = evaluate(state)
+        return [-1, -1, score]
 
 "main  function"
 def main():
