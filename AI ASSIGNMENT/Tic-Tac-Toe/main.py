703bdecbcd14ef9bde07f9f7e1d7e21a4b3ade9e
Sabrina <sanutaayvi@gmail.com>
2021-01-05 15:23:11 +0600

    tic tac  toe finished

--- AI ASSIGNMENT/Tic-Tac-Toe/main.py
+++ AI ASSIGNMENT/Tic-Tac-Toe/main.py
@@ -1,21 +1,37 @@
+from math import inf as infinity
+from random import choice
 import platform
 import time
-from math import inf as infinity
 from os import system
-from random import choice
 
-humen =  -1
-computer = 1
+
+
+HUMAN = -1
+COMPUTER = +1
 board = [
-    [0,0,0],
     [0, 0, 0],
-    [0, 0, 0]
-
+    [0, 0, 0],
+    [0, 0, 0],
 ]
-"check  possibilities  of  wins"
+
+def evaluate(state):
+
+    if wins(state, COMPUTER):
+        score = +1
+    elif wins(state, HUMAN):
+        score = -1
+    else:
+        score = 0
+
+    return score
+
+
+"""
+Possibilities of  wins
+  """
 def wins(state, player):
 
-    win_state = [
+    winState = [
         [state[0][0], state[0][1], state[0][2]],
         [state[1][0], state[1][1], state[1][2]],
         [state[2][0], state[2][1], state[2][2]],
@@ -25,32 +41,26 @@ def wins(state, player):
         [state[0][0], state[1][1], state[2][2]],
         [state[2][0], state[1][1], state[0][2]],
     ]
-    if [player, player, player] in win_state:
+    if [player, player, player] in winState:
         return True
     else:
         return False
 
 
-"game over state"
+"""
+computer  wins  or  human
+  """
 def gameOver(state):
-    return wins(state, humen) or wins(state, computer)
 
+    return wins(state, HUMAN) or wins(state, COMPUTER)
 
-
-def evaluate(state):
-    cells = []
-
-    for x, row in enumerate(state):
-        for y, cell in enumerate(row):
-            if cell == 0:
-                cells.append([x, y])
-
-    return cells
 
 """
-Each empty cell will be added into cells list
-"""
+   empty cell  in  the list
+
+ """
 def emptyCells(state):
+
     cells = []
 
     for x, row in enumerate(state):
@@ -60,71 +70,80 @@ def emptyCells(state):
 
     return cells
 
-"""
-    A move is valid if the chosen cell is empty
-    
-    """
-def validMove(xCoordinate, yCoordinate):
-
-    if [xCoordinate, yCoordinate] in emptyCells(board):
-        return True
-    else:
-        return False
 
 """
-   Player  move  if the coordinates are valid
-    
-    """
-def setMove(xCoordinate, yCoordinate, player):
+valid  move = empty  cells
+  """
+def validMove(x, y):
 
-    if validMove(xCoordinate, yCoordinate):
-        board[xCoordinate][yCoordinate] = player
+    if [x, y] in emptyCells(board):
         return True
     else:
         return False
 
 
-"minimax fuction  will  choose   the  best  move"
+"""
+ move  will  set  if  the  cell  is  valid
+  """
+def setMove(x, y, player):
+
+    if validMove(x, y):
+        board[x][y] = player
+        return True
+    else:
+        return False
+
+
+"""
+  MINIMAX function  for  the best move
+
+   """
 def minimax(state, depth, player):
-    if player == computer:
+
+    if player == COMPUTER:
         best = [-1, -1, -infinity]
     else:
         best = [-1, -1, +infinity]
+
     if depth == 0 or gameOver(state):
         score = evaluate(state)
         return [-1, -1, score]
+
     for cell in emptyCells(state):
-        xCoordinate, yCoordinate = cell[0], cell[1]
-        state[xCoordinate][yCoordinate] = player
+        x, y = cell[0], cell[1]
+        state[x][y] = player
         score = minimax(state, depth - 1, -player)
-        state[xCoordinate][yCoordinate] = 0
-        score[0], score[1] = xCoordinate, yCoordinate
-    if player == computer:
-        if score[2] > best[2]:
-            best = score  # maximum value
+        state[x][y] = 0
+        score[0], score[1] = x, y
+
+        if player == COMPUTER:
+            if score[2] > best[2]:
+                best = score  # maximum value
         else:
             if score[2] < best[2]:
-                best = score  # min value
+                best = score  # minimum value
 
     return best
 
-
 """
-   Clears the console
-   """
+clearing  console
+"""
 def clean():
-    os_name = platform.system().lower()
-    if 'windows' in os_name:
+
+    osName = platform.system().lower()
+    if 'windows' in osName:
         system('cls')
     else:
         system('clear')
- """
-    Print the board on console
-     """
-def render(state, computer_choice, humen_choice):
+
+"""
+ showing board  on  console
+      """
+def render(state, computer_choice, human_choice):
+
 
     chars = {
-        -1: humen_choice,
+        -1: human_choice,
         +1: computer_choice,
         0: ' '
     }
@@ -137,45 +156,33 @@ def render(state, computer_choice, humen_choice):
             print(f'| {symbol} |', end='')
         print('\n' + str_line)
 
- """
-    It calls the minimax function if the depth < 9,
-    else it choices a random coordinate.
-    """
 
-
-def xCoordinate(args):
-    pass
-
-
-def yCoordinate(args):
-    pass
-
-
-def ai_turn( computer_choice, humen_choice):
+def computer_turn(computer_choice, human_choice):
 
     depth = len(emptyCells(board))
     if depth == 0 or gameOver(board):
         return
 
     clean()
-    print(f'Computer turn [{ computer_choice }]')
-    render(board,  computer_choice, humen_choice)
+    print(f'Computer turn [{computer_choice}]')
+    render(board, computer_choice, human_choice)
 
     if depth == 9:
         x = choice([0, 1, 2])
         y = choice([0, 1, 2])
     else:
-        move = minimax(board, depth, computer)
+        move = minimax(board, depth, COMPUTER)
         x, y = move[0], move[1]
 
-    setMove(xCoordinate, yCoordinate, computer)
+    setMove(x, y, COMPUTER)
     time.sleep(1)
 
- """
-    The Human plays choosing a valid move.
-     
-    """
-def human_turn( computer_choice, humen_choice):
+
+"""
+  valid move  for human
+
+   """
+def human_turn(computer_choice, human_choice):
 
     depth = len(emptyCells(board))
     if depth == 0 or gameOver(board):
@@ -190,14 +197,14 @@ def human_turn( computer_choice, humen_choice):
     }
 
     clean()
-    print(f'Human turn [{humen_choice}]')
-    render(board, computer_choice, humen_choice)
+    print(f'Human turn [{human_choice}]')
+    render(board, computer_choice, human_choice)
 
     while move < 1 or move > 9:
         try:
             move = int(input('Use numpad (1..9): '))
             coord = moves[move]
-            can_move = setMove(coord[0], coord[1], humen)
+            can_move = setMove(coord[0], coord[1], HUMAN)
 
             if not can_move:
                 print('Bad move')
@@ -210,19 +217,19 @@ def human_turn( computer_choice, humen_choice):
 
 
 
-"main  function"
+"""Main  function """
 def main():
 
     clean()
-    humen_choice = ''  # X or O
+    human_choice = ''  # X or O
     computer_choice = ''  # X or O
     first = ''  # if human is the first
 
     # Human chooses X or O to play
-    while humen_choice != 'O' and humen_choice != 'X':
+    while human_choice != 'O' and human_choice != 'X':
         try:
             print('')
-            humen_choice = input('Choose X or O\nChosen: ').upper()
+            human_choice = input('Choose X or O\nChosen: ').upper()
         except (EOFError, KeyboardInterrupt):
             print('Bye')
             exit()
@@ -230,7 +237,7 @@ def main():
             print('Bad choice')
 
     # Setting computer's choice
-    if humen_choice == 'X':
+    if human_choice == 'X':
         computer_choice = 'O'
     else:
         computer_choice = 'X'
@@ -249,26 +256,26 @@ def main():
     # Main loop of this game
     while len(emptyCells(board)) > 0 and not gameOver(board):
         if first == 'N':
-            ai_turn(computer_choice , humen_choice)
+            computer_turn(computer_choice, human_choice)
             first = ''
 
-        human_turn(computer_choice , humen_choice)
-        ai_turn(computer_choice , humen_choice)
+        human_turn(computer_choice, human_choice)
+        computer_turn(computer_choice, human_choice)
 
     # Game over message
-    if wins(board, humen):
+    if wins(board, HUMAN):
         clean()
-        print(f'Human turn [{humen_choice}]')
-        render(board, computer_choice , humen_choice)
+        print(f'Human turn [{human_choice}]')
+        render(board, computer_choice, human_choice)
         print('YOU WIN!')
-    elif wins(board, computer):
+    elif wins(board, COMPUTER):
         clean()
         print(f'Computer turn [{computer_choice}]')
-        render(board,computer_choice , humen_choice)
+        render(board, computer_choice, human_choice)
         print('YOU LOSE!')
     else:
         clean()
-        render(board,computer_choice , humen_choice)
+        render(board, computer_choice, human_choice)
         print('DRAW!')
 
     exit()
